26111bee847ac1b848722e05857db32d410a0c58
Pedro <82657264+Pedrob01@users.noreply.github.com>
2023-04-21 11:16:44 +0100

    mppt adjusted
    Matlab mppt adjusted for the groups requirements

--- mpptm.m
+++ mpptm.m
@@ -7,14 +7,57 @@ if isempty(Dprev)
     Vprev = 190;
     Pprev = 2000;
 end
+%initialize parametersfunction D = mpptm(Vpv,Ipv)
+
+Dmin = 0.05;
+Dmax = 0.95;
+persistent Dprev Pprev Vprev ;
+
+if isempty(Dprev)
+    Dprev = 0.95;
+    Vprev = 0;
+    Pprev = 0;
+end
 %initialize parameters
+deltaD = 0.02;
+
+% initalize measured array power
+Ppv = Vpv*Ipv;
+
+if((Ppv - Pprev) ~= 0) 
+    if((Ppv - Pprev) > 0) 
+        if((Vpv-Vprev) > 0) 
+            D = Dprev - deltaD;
+        else
+            D = Dprev + deltaD;
+        end
+    else
+        if((Vpv-Vprev) > 0)
+            D = Dprev + deltaD;
+        else
+            D = Dprev - deltaD;
+        end
+    end
+else
+    D = Dprev;
+end
+
+%update values
+if D >= Dmax || D < Dmin
+    D = Dprev;
+end
+Dprev = D;
+Vprev = Vpv;
+Pprev = Ppv;
+
+
 deltaD = 0.001;
 
 % initalize measured array power
 Ppv = Vpv*Ipv;
 
 
-if(Ppv - Pprev) ~= 0
+if(Ppv - Pprev) ~= 0 || Ppv-Pprev
     if(Ppv - Pprev) > 0
         if(Vpv-Vprev) > 0
             D = Dprev - deltaD;
