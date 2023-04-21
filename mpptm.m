c4777222309ee8b27dcfdddcabf61074345d9501
Pedro <82657264+Pedrob01@users.noreply.github.com>
2023-04-21 11:18:16 +0100

    Oops
    Wrong file uploaded last commit

--- mpptm.m
+++ mpptm.m
@@ -1,14 +1,5 @@
 function D = mpptm(Vpv,Ipv)
 
-persistent Dprev Pprev Vprev
-
-if isempty(Dprev)
-    Dprev = 0.7;
-    Vprev = 190;
-    Pprev = 2000;
-end
-%initialize parametersfunction D = mpptm(Vpv,Ipv)
-
 Dmin = 0.05;
 Dmax = 0.95;
 persistent Dprev Pprev Vprev ;
@@ -50,33 +41,3 @@ Dprev = D;
 Vprev = Vpv;
 Pprev = Ppv;
 
-
-deltaD = 0.001;
-
-% initalize measured array power
-Ppv = Vpv*Ipv;
-
-
-if(Ppv - Pprev) ~= 0 || Ppv-Pprev
-    if(Ppv - Pprev) > 0
-        if(Vpv-Vprev) > 0
-            D = Dprev - deltaD;
-        else
-            D = Dprev + deltaD;
-        end
-    else
-        if(Vpv-Vprev) > 0
-            D = Dprev + deltaD;
-        else
-            D = Dprev - deltaD;
-        end
-    end
-else
-    D = Dprev;
-end
-%update values
-Dprev = D;
-Vprev = Vpv;
-Pprev = Ppv;
-
-end
